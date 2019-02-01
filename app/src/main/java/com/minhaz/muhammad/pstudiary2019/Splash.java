6eaec9759fcb13a060c7024d09b47bde5a700419
muhammadminhaz <mminhazurrahman0@gmail.com>
2019-02-01 18:22:57 +0600

    FIrst commit

---
+++ app/src/main/java/com/minhaz/muhammad/pstudiary2019/Splash.java
@@ -0,0 +1,27 @@
+package com.minhaz.muhammad.pstudiary2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
