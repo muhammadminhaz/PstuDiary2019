9d4db69caeaf13294504ea8fed169de6fdbc438a
muhammadminhaz <mminhazurrahman0@gmail.com>
2019-02-19 20:39:44 +0600

    commit9

--- app/src/main/java/com/minhaz/muhammad/pstudiary2019/MainActivity.java
+++ app/src/main/java/com/minhaz/muhammad/pstudiary2019/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.setOffscreenPageLimit(4);
+        mViewPager.setOffscreenPageLimit(3);
 
 
 
